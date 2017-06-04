caca01eabb63140202197a215c5633f33eac448f
luigiramos <luigiramos166@gmail.com>
2017-06-04 12:56:18 +0800

    Added Basic Functionality
    - bot and user responses
    - Keyword matcher
    - 3 branches (no functionality yet)

---
+++ src/ChatBot.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class ChatBot {
+	private String botName = "Course Bot";
+	Scanner scan = new Scanner(System.in);
+	String userResponse;
+	String highPayKeywords[] = {"high pay", "lots of money", "rich", "pays well"};
+	String passionKeywords[] = {"interests", "interest", "passion", "passions"};
+	String parentsKeywords[] = {"parents", "parent", "dad", "mom", "father", "mother"};
+	
+	public void start(){
+		botSay("Hi, my name is "+botName+" and I'm here to help you look for a college course that suits you.");
+		botSay("What kind of course are you looking for?");
+		
+		userResponse = userSay();
+		if (keywordMatcher(userResponse, highPayKeywords)){
+			highPayBranch();
+		}
+		if (keywordMatcher(userResponse, passionKeywords)){
+			passionBranch();
+		}
+		if (keywordMatcher(userResponse, parentsKeywords)){
+			parentsBranch();
+		}
+	}
+	
+	private void highPayBranch() {
+		System.out.println("high pay");
+		
+	}
+
+	private void passionBranch() {
+		System.out.println("passion");
+		
+	}
+
+	private void parentsBranch() {
+		System.out.println("parents");
+		
+	}
+
+	public void botSay(String s){ //Bot response
+		System.out.println(botName+": "+s+"\n");
+	}
+	
+	public String userSay(){ //user response
+		System.out.print("You: ");
+		String response = scan.nextLine();
+		return response;
+	}
+	
+	public boolean keywordMatcher(String response, String keywords[]){
+		for(int i = 0; i<keywords.length; i++){
+			if (response.contains(keywords[i])){
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		ChatBot cb = new ChatBot();
+		cb.start();
+	}
+
+}
